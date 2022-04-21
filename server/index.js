2ef9c7931a9602eae72953afa11bca4d4a98c0e5
Lakindu Boteju <lakinduboteju@gmail.com>
2022-04-21 14:11:55 +0530

    Using multiple workers in SocketIO server.

--- server/index.js
+++ server/index.js
@@ -1,67 +1,84 @@
-const config          = require('./config');
+const config = require('./config');
+const sticky = require('socketio-sticky-session');
+const cluster = require('cluster');
 
-const app             = require('express')();
+const stickyOptions = {
+  proxy: true,
+  ignoreMissingHeader: true,
+};
 
-const server          = require('http').createServer(app);
+sticky(stickyOptions, () => {
+  // This code will be executed only in slave workers
+  const workerId = cluster.worker.id;
 
-const io              = require('socket.io')(server, {
-                          pingInterval: 25000,
-                          pingTimeout: 10000,
-                        }).listen(config.socketIoPort); // SocketIO server port taken from config
+  const app = require('express')();
 
-const redisClient     = require('redis').createClient(
-                          config.redisServerPort,
-                          config.redisServerHost
-                        ); // Redis server should already be running
+  const server = require('http').createServer(app);
 
-const messageHistory  = require('./messageHistory');
+  const io = require('socket.io')(server, {
+    pingInterval: 25000,
+    pingTimeout: 10000,
+  });
 
-// Redis client ready handler
-redisClient.on('ready', () => {
-  // SocketIO connection handler
-  io.sockets.on('connection', async socket => {
-    console.debug('New Connection :', socket.id);
+  // Redis server should already be running
+  const redisClient = require('redis').createClient(
+    config.redisServerPort,
+    config.redisServerHost
+  );
 
-    // SocketIO socket message handler
-    socket.on('message', async (msg, ack) => {
-      // Acknowledge to received messages
-      if (ack) {
-        try {
-          let inserted = await messageHistory.insertIfNotExist(redisClient, socket.id, msg);
-          // Inserted in to history?
-          if (inserted) {
-            ack(JSON.stringify({status: 1, msg: 'Message received.'})); // Acknowledge and proceed further
-          } else {
-            // Message already exists in history
-            return ack(JSON.stringify({status: 0, msg: 'Message is already received.'})); // Acknowledge and no need to proceed
+  const messageHistory = require('./messageHistory');
+
+  // Redis client ready handler
+  redisClient.on('ready', () => {
+    console.debug(`WORKER ${workerId} started`);
+
+    // SocketIO connection handler
+    io.sockets.on('connection', async socket => {
+      console.debug('New Connection :', socket.id);
+
+      // SocketIO socket message handler
+      socket.on('message', async (msg, ack) => {
+        // Acknowledge to received messages
+        if (ack) {
+          try {
+            let inserted = await messageHistory.insertIfNotExist(redisClient, socket.id, msg);
+            // Inserted in to history?
+            if (inserted) {
+              ack(JSON.stringify({status: 1, msg: 'Message received.'})); // Acknowledge and proceed further
+            } else {
+              // Message already exists in history
+              return ack(JSON.stringify({status: 0, msg: 'Message is already received.'})); // Acknowledge and no need to proceed
+            }
+          } catch (err) {
+            console.error('Failed to insert persistent message in to message history and acknowledge :', err);
           }
-        } catch (err) {
-          console.error('Failed to insert persistent message in to message history and acknowledge :', err);
         }
-      }
 
-      // wait for random time (between 0 and 5 secs)
-      await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 5) * 1000));
-      // console.debug('MSG :', msg);
-    });
+        // wait for random time (between 0 and 5 secs)
+        await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 5) * 1000));
+        console.debug(`WRK_ID : ${workerId} MSG : ${msg}`);
+      });
 
-    // SocketIO socket disconnect handler
-    socket.on('disconnect', async reason => {
-      // Delete message history
-      await messageHistory.delete(redisClient, socket.id);
-      console.debug('Disconnected :', socket.id, ` : ${reason}`);
-    });
+      // SocketIO socket disconnect handler
+      socket.on('disconnect', async reason => {
+        // Delete message history
+        await messageHistory.delete(redisClient, socket.id);
+        console.debug('Disconnected :', socket.id, ` : ${reason}`);
+      });
 
-    // SocketIO socket error handler
-    socket.on('error', async err => {
-      console.error('Connection Error :', err);
+      // SocketIO socket error handler
+      socket.on('error', async err => {
+        console.error('Connection Error :', err);
+      });
     });
   });
 
-  console.debug('SocketIO server listening at port', config.socketIoPort);
-});
+  // Redis client error handler
+  redisClient.on('error', err => {
+    console.error("Redis client error :", err);
+  });
 
-// Redis client error handler
-redisClient.on('error', err => {
-  console.error("Redis client error :", err);
+  return server;
+}).listen(config.socketIoPort, () => {
+  console.debug(`SocketIO server listening at port ${config.socketIoPort}`);
 });
